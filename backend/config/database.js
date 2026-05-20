99ade4b55b23c97234f2115f270732a6bce6ae80
alejo28121 <grajalesvargasalejandro@gmail.com>
2026-05-19 19:38:20 -0500

    Fix db pool

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,16 +2,13 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-    host: process.env.DBHOST,
-    port: process.env.DBPORT || 5432,
-    user: process.env.DBUSER,
-    password: process.env.DBPASSWORD,
-    database: process.env.DB,
-    ssl: { rejectUnauthorized: false },
-
-    max: 10, 
-    idleTimeoutMillis: 30000, 
-    connectionTimeoutMillis: 5000, 
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    },
+    max: 10,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 5000,
 });
 
 pool.on("error", (err) => {
